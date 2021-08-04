f83174074f0e9a42fee895fa041dd297bf10db2c
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-04 10:19:49 +0530

    [Akshay] Added ->Aggregate Functions After Table Splitting

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -41,3 +41,36 @@ insert into Payroll values
 (1, '2018-01-03', 100000, 10000, 90000, 1000, 89000),
 (2, '2019-11-13', 200000, 10000, 190000,3000,187000),
 (3, '2020-05-21', 300000, 20000, 280000, 5000, 275000);
+
+---------------UC12- Aggregate Functions After Table Splitting----------------------------
+
+--Aggregate operationsby gender
+
+--Total of basic pay by gender
+select emp.gender, Sum(payroll.Basic_pay) AS Total_Pay
+from Payroll payroll inner join employee emp
+on payroll.Id = emp.Id
+group by gender;
+
+--Average of basic pay by gender
+select emp.gender, AVG(payroll.Basic_pay) AS Average_Pay 
+from Payroll payroll inner join employee emp
+on payroll.Id = emp.Id
+group by gender;
+
+--Count number of employees by gender
+select gender, Count(Name) as Total_Count
+from employee 
+group by gender;
+
+--Minimum salary grouped by gender
+select gender,MIN(payroll.Basic_pay) as Min_Pay
+from Payroll payroll inner join employee emp
+on payroll.Id = emp.Id
+group by gender;
+
+--Maximum salary grouped by gender
+select gender,MAX(payroll.Basic_pay) as Max_Pay
+from Payroll payroll inner join employee emp
+on payroll.Id = emp.Id
+group by gender;
